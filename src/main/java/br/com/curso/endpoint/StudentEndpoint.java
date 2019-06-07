d9245cd800273f93d1cddc18b9caf64b3fc854ac
marildo <marildocesar@hotmail.com>
2019-06-06 22:25:38 -0300

    Aula 06 - Implementando metodo Save

--- src/main/java/br/com/curso/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/curso/endpoint/StudentEndpoint.java
@@ -6,10 +6,7 @@ import br.com.curso.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,18 +27,21 @@ public class StudentEndpoint {
         return new ResponseEntity<>(Student.studentsList, HttpStatus.OK);
     }
 
-
     @RequestMapping(method = RequestMethod.GET,path = "/{id}")
     public ResponseEntity<?> readById(@PathVariable("id") int id){
         Student student = new Student();
         student.setId(id);
-
         int index = Student.studentsList.indexOf(student);
-
         if(index ==-1)
              return  new ResponseEntity<>(new CustonErrorType("Student not found"),HttpStatus.NOT_FOUND);
-
         return new ResponseEntity<>(Student.studentsList.get(index),HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student){
+        Student.studentsList.add(student);
+        return new ResponseEntity<>(student,HttpStatus.OK);
 
     }
+
 }
