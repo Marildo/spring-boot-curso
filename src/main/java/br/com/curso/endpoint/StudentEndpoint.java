777d7536578d93169cd258a524acde69fca499f6
MCesar <marildocesar@hotmail.com>
2019-06-07 17:43:42 -0300

    Aula 08 - Add conexao com Mysql

--- src/main/java/br/com/curso/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/curso/endpoint/StudentEndpoint.java
@@ -1,57 +1,53 @@
 package br.com.curso.endpoint;
 
-import br.com.curso.Util.DateUtil;
 import br.com.curso.error.CustonErrorType;
 import br.com.curso.model.Student;
+import br.com.curso.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("students")
 public class StudentEndpoint {
 
-    @Autowired
-    private DateUtil dateUtil;
+    private final StudentRepository dao;
 
-    public StudentEndpoint(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
+    @Autowired
+    public StudentEndpoint(StudentRepository dao) {
+        this.dao = dao;
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> readAll() {
-        return new ResponseEntity<>(Student.studentsList, HttpStatus.OK);
+        return new ResponseEntity<>(dao.findAll(), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> readById(@PathVariable("id") int id){
-        Student student = new Student();
-        student.setId(id);
-        int index = Student.studentsList.indexOf(student);
-        if(index ==-1)
-             return  new ResponseEntity<>(new CustonErrorType("Student not found"),HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(Student.studentsList.get(index),HttpStatus.OK);
+    public ResponseEntity<?> readById(@PathVariable("id") Long id) {
+        Optional<Student> opt = dao.findById(id);
+
+        if (opt.isPresent())
+            return new ResponseEntity<>(opt.get(), HttpStatus.OK);
+        return new ResponseEntity<>(new CustonErrorType("Student not found"), HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody Student student){
-        Student.studentsList.add(student);
-        return new ResponseEntity<>(student,HttpStatus.OK);
+    public ResponseEntity<?> create(@RequestBody Student student) {
+        return new ResponseEntity<>(dao.save(student), HttpStatus.OK);
     }
 
     @PutMapping
-    public ResponseEntity<?> update(@RequestBody Student student){
-        Student.studentsList.add(student);
-        return new ResponseEntity<>(student,HttpStatus.OK);
+    public ResponseEntity<?> update(@RequestBody Student student) {
+        return new ResponseEntity<>(dao.save(student), HttpStatus.OK);
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> delete(@RequestBody Student student){
-        Student.studentsList.remove(student);
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+         dao.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
