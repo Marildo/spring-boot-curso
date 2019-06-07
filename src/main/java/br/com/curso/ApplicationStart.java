3dbd4ff8867dc4dbc884e6dc47e2a855ea2715b9
MCesar <marildocesar@hotmail.com>
2019-06-07 17:47:26 -0300

    Aula 08 - Add conexao com Mysql [2]

--- src/main/java/br/com/curso/ApplicationStart.java
+++ src/main/java/br/com/curso/ApplicationStart.java
@@ -3,12 +3,16 @@ package br.com.curso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 /*
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"br.com.curso.*"})
 @Configuration
  ou apenas @SpringBootApplication
  */
+
 
 @SpringBootApplication
 public class ApplicationStart {
---
+++ src/main/java/br/com/curso/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package br.com.curso.repository;
+
+import br.com.curso.model.Student;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends CrudRepository<Student,Long> {
+
+    List<String> findByName(String name);
+}
